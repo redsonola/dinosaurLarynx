2b946f41106ae13877597c86d2b4c6e2433b3e95
redsonola@gmail.com <redsonola@gmail.com>
2023-06-22 15:49:04 -0700

    added line breaks

--- webSyrinx/my-app/src/routes/Header.svelte
+++ webSyrinx/my-app/src/routes/Header.svelte
@@ -34,8 +34,8 @@
 </header>
 
 	<directions><p>
-		Click anywhere in the window  to start the sound and blow into the microphone! <br />  
-		You can change the tension on the vocal cords by moving your mouse! 
+		Click anywhere in the window  to start the sound and blow into the microphone! <br /><br />  
+		You can change the tension on the vocal cords by moving your mouse! <br /><br/>
 
 		Changing the tension affects the pitch and timbre. The lower you move your mouse (you can scroll down) the lower the tension, and generally, the lower the pitch. The higher your mouse moves, the higher the pitch. In addition, the more or less you accelerate your breath, this also has an effect on pitch, timbre, and volume.</p>
 	</directions>
