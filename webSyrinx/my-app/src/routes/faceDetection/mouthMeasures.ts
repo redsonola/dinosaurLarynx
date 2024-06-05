f15bd2e55321651e3f90ae7528ca8e3ffc314dd2
redsonola@gmail.com <redsonola@gmail.com>
2024-06-05 17:22:35 -0500

    new rescale

--- webSyrinx/my-app/src/routes/faceDetection/mouthMeasures.ts
+++ webSyrinx/my-app/src/routes/faceDetection/mouthMeasures.ts
@@ -6,10 +6,10 @@ const mouthDataHeader = "Wideness Min, Wideness Max, Open Min, Open Max";
 export const mouthDataFile : DataFileCSV = new DataFileCSV(mouthDataFileName, mouthDataHeader);
 
 //updated with better values, depth normalization
-export var wideMin = 10;   //0.14866; //0.07;
-export var wideMax = 250; //0.34627; //0.12;
-export var mouthAreaMin = 0.05; //0.000291; //0.0; 
-export var mouthAreaMax = 75; //0.021153; //0.005480977000770437
+export var wideMin = 0.07111;  //10;   //0.14866; //0.07;
+export var wideMax = 0.08; //250; //0.34627; //0.12;
+export var mouthAreaMin = 0.002585; //0.05; //0.000291; //0.0; 
+export var mouthAreaMax = 0.0213; //75; //0.021153; //0.005480977000770437
 
 //camera position:
 //9cm back from mouthpiece, centered horizontally
