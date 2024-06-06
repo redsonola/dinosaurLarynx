39e026e361e834ec2f889f3403e793bd34c62894
redsonola@gmail.com <redsonola@gmail.com>
2024-06-06 14:48:39 -0500

    training mouth wideness, format

--- webSyrinx/my-app/src/routes/mouthTracking.svelte
+++ webSyrinx/my-app/src/routes/mouthTracking.svelte
@@ -140,6 +140,38 @@
 	  align-items: center;
 	  background-color: #007f8b;
 	}
+
+	.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 25px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  background: #04AA6D;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  background: #04AA6D;
+  cursor: pointer;
+}
+
 	  </style>
 	
 	<!-- </svelte:head> --
@@ -193,7 +225,7 @@
 				<button id="editMouthValueAutoFill"> Fill values with Raw Minimums and Maximums for Mouth Wideness/Openness  </button> <br />
 				<button id="submitEditMouthConfig"> Update Mouth Tracking Scaling for Mouth Wideness/Openness</button> <br />
 
-				<p>Select output for training:</p>
+				<p>Training mouth wideness across depth values:</p>
 				<div class="slidecontainer">
 					<input type="range" min="0.0" max="100.0" value="0.0" class="slider" id="wideOut" name="wideOut">
 					<label for="wideOut">Wideness value</label><br>
