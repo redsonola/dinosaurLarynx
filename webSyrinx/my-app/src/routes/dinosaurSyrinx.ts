9b3186920be356f896f811edb29c0d7294938f2a
redsonola@gmail.com <redsonola@gmail.com>
2024-06-11 17:13:21 -0500

    added volume

--- webSyrinx/my-app/src/routes/dinosaurSyrinx.ts
+++ webSyrinx/my-app/src/routes/dinosaurSyrinx.ts
@@ -830,7 +830,7 @@ export function trachealSyrinx() {
         const limiter = new Tone.Limiter();
         const compressor = new Tone.Compressor();
         const gain = new Tone.Gain(50); //to do -- make visible to UI
-        const vol = new Tone.Volume(3);
+        const vol = new Tone.Volume(8);
 
         membrane = new SyrinxMembraneFS({ pG: 0.0 }); //needs to be global.. 
 
